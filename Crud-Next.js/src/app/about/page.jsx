257c4a875262f677221d5d4aedf64024c7536669
Cristian Garcia Mesa <garciamesacris543@gmail.com>
2024-07-22 16:01:04 -0500

    Apuntes Next.js

---
+++ Crud-Next.js/src/app/about/page.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <section className="container mx-auto">
+            <h3 className="text-4xl font-bold my-5">About</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptatum minus, quaerat atque aperiam excepturi vero asperiores numquam, nobis, fugiat animi. In fuga, vel dolor at laborum illum officia nam unde. Nostrum magnam similique ipsam nemo eveniet, saepe dolorum est explicabo atque suscipit omnis itaque consectetur perferendis. Ea rerum labore nam culpa et, maxime repudiandae reiciendis consectetur omnis quae dolore quos. Voluptatum vitae asperiores officiis similique corrupti, iure, fuga provident expedita veniam ut magni beatae impedit possimus. Aliquid itaque ea voluptas architecto, necessitatibus aliquam? Est, dolor rem id at repudiandae consequuntur facilis qui iure. Laboriosam libero officia quod illum maiores!</p>
+        </section>
+    )
+};
+
+export default AboutPage;
---
+++ Crud-Next.js/src/app/page.jsx
@@ -0,0 +1,24 @@
+import TaskCard from "@/components/TaskCard";
+import { prisma } from "@/libs/prisma";
+
+async function loadTasks() {
+  return await prisma.task.findMany()
+}
+
+
+const HomePage = async () => {
+
+  const tasks = await loadTasks()
+
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map(task => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </section>
+  )
+};
+
+export default HomePage;
