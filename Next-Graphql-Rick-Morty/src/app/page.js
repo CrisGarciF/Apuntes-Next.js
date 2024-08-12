1e4f92461bfa773fc2047d8cb91cbf5ea7385f09
Cristian Garcia Mesa <garciamesacris543@gmail.com>
2024-08-12 12:49:33 -0500

    Apuntes Next.js

---
+++ Next-Graphql-Rick-Morty/src/app/page.js
@@ -0,0 +1,39 @@
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
+
+
+async function loadData() {
+  const {data} = await getClient().query({
+    query: gql`
+      query {
+        characters(page: 1) {
+          results {
+            id
+            name
+            image
+          }
+        }
+      }
+    `,
+  })
+
+  return data.characters.results;
+}
+
+const HomePage = async () => {
+
+  const characters = await loadData();
+
+  return (
+    <div className="grid grid-cols-3">
+      {characters.map(character => (
+        <div key={character.id}>
+          <h3> {character.name} </h3>
+          <img  src={character.image} alt={character.name} />
+        </div>
+      ))}
+    </div>
+  )
+};
+
+export default HomePage;
